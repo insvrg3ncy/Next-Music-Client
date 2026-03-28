099a71f8447edc0df1427753919f4e2f93c572f2
Diramix <danyagranny10@proton.me>
2026-03-28 19:54:41 +0300

    fix(settings): maximize/minimize button

--- src/lib/window/settingsWindow/preload.cjs
+++ src/lib/window/settingsWindow/preload.cjs
@@ -1,46 +1,28 @@
 // src/windows/settingsPreload.js
 const { contextBridge, ipcRenderer } = require("electron");
-
 contextBridge.exposeInMainWorld("electronAPI", {
-    /** Получить версии приложения, Electron, Chromium, Node */
     getVersions: () => ipcRenderer.invoke("settings:get-versions"),
-
-    /** Получить текущий конфиг */
     loadConfig: () => ipcRenderer.invoke("settings:load-config"),
-
-    /** Сохранить весь конфиг сразу (вызывается при каждом изменении поля) */
     saveConfig: (config) => ipcRenderer.invoke("settings:save-config", config),
-
-    /** Развернуть / восстановить окно */
     toggleMaximize: () => ipcRenderer.send("settings:toggle-maximize"),
-
-    /** Подписка на смену состояния maximize */
-    onMaximizeChange: (cb) =>
+    onMaximizeChange: (cb) => {
+        ipcRenderer.removeAllListeners("settings:maximize-changed");
         ipcRenderer.on("settings:maximize-changed", (_event, isMaximized) =>
             cb(isMaximized),
-        ),
-
-    /** Свернуть окно настроек */
+        );
+    },
     minimizeWindow: () => ipcRenderer.send("settings:minimize"),
-
-    /** Закрыть окно настроек */
     closeWindow: () => ipcRenderer.send("settings:close"),
-
-    /** Открыть папку с аддонами */
     openAddonsFolder: () => ipcRenderer.send("settings:open-addons-folder"),
-
-    /** Перезапустить приложение (relaunch + exit) */
     restartApp: () => ipcRenderer.send("settings:restart-app"),
-
     loadLangStrings: () => ipcRenderer.invoke("settings:load-lang-strings"),
-
     getLangList: () => ipcRenderer.invoke("settings:get-lang-list"),
-
     setLanguage: (langCode) =>
         ipcRenderer.send("settings:set-language", langCode),
-
-    onLanguageChange: (cb) =>
+    onLanguageChange: (cb) => {
+        ipcRenderer.removeAllListeners("settings:language-changed");
         ipcRenderer.on("settings:language-changed", (_event, strings) =>
             cb(strings),
-        ),
+        );
+    },
 });
